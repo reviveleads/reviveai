fb6adf0e4b9cd77e15fddb9017a6f07c3a1d7238
reviveleads <jhanley1022@gmail.com>
2026-04-19 21:05:52 -0400

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -25,7 +25,7 @@ export default function LoginPage() {
       setLoading(false)
       return
     }
-    window.location.href = '/dashboard'
+    window.location.href = 'https://app.reviveleads.net/dashboard'
   }
 
   return (
